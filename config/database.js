252333cd6d8e3029dcc553eb187fb088d2b4374b
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-10-04 11:43:06 -0500

    added environment variables

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
-module.exports = {
-    
-    // mongoDbUrl: 'mongodb://localhost:27017/cms'
-
-    mongoDbUrl: 'mongodb://cms-user:mojddev12@ds259305.mlab.com:59305/cms'
-
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod-database');
+} else {
+    module.exports = require('./dev-database');
+}
