5d5a9d6797b0c679a310f6b441d940775d37bb69
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-10-04 13:48:17 -0500

    Deleting post with  comments

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -174,11 +174,18 @@ router.put('/edit/:id', (req, res)=>{
 
 router.delete('/:id', (req, res)=>{
     Post.findOneAndDelete({_id: req.params.id})
+        .populate('comments')
         .then(post=>{
             fs.unlink(uploadDir + post.file, (err)=>{
-                post.remove();
-                req.flash('sucess_message', 'Post was succesfully deleted');
-                res.redirect('/admin/posts');
+                if(!post.comments.length < 1){
+                    post.comments.forEach(comment => {
+                        comment.remove();
+                    });
+                }
+                post.remove().then(postRemoved=>{
+                    req.flash('sucess_message', 'Post was succesfully deleted');
+                    res.redirect('/admin/posts');
+                });
         });
     });
 });
