e9730d2286f8bf2aa6a98b866a279439880cdb5e
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-10-04 21:08:38 -0500

    Building Blog Part 1.2

--- models/Post.js
+++ models/Post.js
@@ -7,6 +7,10 @@ const PostSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'categories'
     },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
 
     title: {
         type: String,
