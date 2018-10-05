979b285f15a2e2d80ef1265f02da943a5f69a44b
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-10-04 23:03:25 -0500

    Allowing to comment if user is login

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(passport.session());
 // Local Variables using Middleware
 
 app.use((req, res, next)=>{
-    res.locals.LoggedUser = req.user || null;
+    res.locals.user = req.user || null;
     res.locals.success_message = req.flash('success_message');
     res.locals.error_message = req.flash('error_message');
     res.locals.form_errors = req.flash('form_errors');
